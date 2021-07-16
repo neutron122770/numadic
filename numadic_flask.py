1d020ceca905cd8afd2e16e49e4841b1527abb65
neutron122770 <87530228+neutron122770@users.noreply.github.com>
2021-07-16 19:01:24 +0530

    Update numadic_flask.py

--- numadic_flask.py
+++ numadic_flask.py
@@ -2,8 +2,8 @@ from flask import Flask, request, make_response
 app = Flask(__name__)
 @app.route('/hello/', methods=['GET', 'POST'])
 def welcome():
-    lat = float(request.args.get("lat"))
-    lon = float(request.args.get("lon"))
+    start_time = int(request.args.get("start_time"))
+    end_time = int(request.args.get("end_time"))
     
     import argparse
     import pandas as pd
@@ -29,28 +29,31 @@ def welcome():
         r = 6371 # Radius of earth in kilometers. Use 3956 for miles
         return c * r
 
-
     db = pd.DataFrame()
     with zipfile.ZipFile('C:/Users/Rakesh/Downloads/test_zip.zip') as zip:
         for i in df.vehicle_number.unique():  
             with zip.open('test%s.csv'%(i)) as myZip: #assumption:zipped csv files are stored in some way where vehicle number is present in file name
                 df1 = pd.read_csv(myZip)  
+                df1['tis'] = df1.tis[df1.tis >= start_time]
+                df1['tis'] = df1.tis[df1.tis <= end_time]
+                if(df1.tis.isna().sum() == df1.lic_plate_no.count()):
+                    print("No data available for provided time period")
+                else: 
+                    df2 = df1.groupby(['lic_plate_no']).agg({'tis' : [np.min, np. max] , 'osf' : np.sum, 'spd' : np.mean}).reset_index() #calculate KPIs for one vechile number
+                    df2.columns = ['lic_plate_no','min_ts','max_ts','no_osf','avg_speed']
 
-                df2 = df1.groupby(['lic_plate_no']).agg({'tis' : [np.min, np. max] , 'osf' : np.sum, 'spd' : np.mean}).reset_index() #calculate KPIs for one vechile number
-                df2.columns = ['lic_plate_no','min_ts','max_ts','no_osf','avg_speed']
+                    df3 = pd.merge(df1, df2, how = 'inner', left_on= ['lic_plate_no','tis'], right_on=['lic_plate_no','min_ts'])  #get start lat lon by joining with min starttime for a vehicle
+                    df3 = df3[['lic_plate_no','lat','lon','max_ts','no_osf',  'avg_speed']]
+                    df3.columns = ['lic_plate_no','lat1','lon1', 'max_ts','no_osf','avg_speed']
+                    df4 = pd.merge(df1, df3, how = 'inner', left_on= ['lic_plate_no','tis'], right_on=['lic_plate_no','max_ts'])  #get end lat lom by joining with max starttime for a vehicle
 
-                df3 = pd.merge(df1, df2, how = 'inner', left_on= ['lic_plate_no','tis'], right_on=['lic_plate_no','min_ts'])  #get start lat lon by joining with min starttime for a vehicle
-                df3 = df3[['lic_plate_no','lat','lon','max_ts','no_osf',  'avg_speed']]
-                df3.columns = ['lic_plate_no','lat1','lon1', 'max_ts','no_osf','avg_speed']
-                df4 = pd.merge(df1, df3, how = 'inner', left_on= ['lic_plate_no','tis'], right_on=['lic_plate_no','max_ts'])  #get end lat lom by joining with max starttime for a vehicle
-
-                df4['distance'] = haversine(df4.lon, df4.lat, df4.lon1, df4.lat1) # calculate distance between start and end points for a vehicle
-                df4 = df4[['lic_plate_no','distance','no_osf',  'avg_speed']]
-                df5 = df4.merge(df_grouped, left_on='lic_plate_no', right_on='vehicle_number') # join with vehicle details file
-                df5.columns = ['lic_plate_no','distance','no_osf',  'avg_speed','vehicle_number', 'transporter_name', 'no_trips']
-                df5 = df5[['lic_plate_no','distance','no_trips',   'avg_speed', 'transporter_name','no_osf']] #get required columns from join 
-                db = db.append(df5) #append all individual vehicle number KPIs
- 
+                    df4['distance'] = haversine(df4.lon, df4.lat, df4.lon1, df4.lat1) # calculate distance between start and end points for a vehicle
+                    df4 = df4[['lic_plate_no','distance','no_osf',  'avg_speed']]
+                    df5 = df4.merge(df_grouped, left_on='lic_plate_no', right_on='vehicle_number') # join with vehicle details file
+                    df5.columns = ['lic_plate_no','distance','no_osf',  'avg_speed','vehicle_number', 'transporter_name', 'no_trips']
+                    df5 = df5[['lic_plate_no','distance','no_trips',   'avg_speed', 'transporter_name','no_osf']] #get required columns from join 
+                    db = db.append(df5) #append all individual vehicle number KPIs
+     
     #adding excel to buffer
     buffer = io.BytesIO()
     db.to_excel(buffer)
@@ -60,7 +63,8 @@ def welcome():
     resp.headers["Content-Type"] = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
     #resp is desired excel
     return resp
- 
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
